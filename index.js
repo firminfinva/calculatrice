5db99c2c85ff9ca75a9e7dd279c67c18b2eb1a4a
kayembejenny <kayembejenny2510@gmail.com>
2023-09-27 10:38:05 +0200

    the commit of function div

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function calculatrice(term1 , operation, term2){
 //function operation
 }
  // function to create add, subs, div, mult
+ function div(a,b){
+    return a/b;
+ }
 
 let response = calculatrice(4, "*", 2)
 console.log(response);
