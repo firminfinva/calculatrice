03c635fb1c998a4f4296f158c730ea91ebaf22c5
firmin nagnduli <finganduli@gmail.com>
2023-09-27 09:37:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function calculatrice(term1 , operation, term2){
+//function operation
+}
+ // function to create add, subs, div, mult
+
+let response = calculatrice(4, "*", 2)
+console.log(response);
+
+
+
+
